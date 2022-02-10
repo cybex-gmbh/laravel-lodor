e9424c7ad382671f96274bef5707328b1c2cf666
lupinitylabs <info@lupinitylabs.com>
2022-02-10 13:30:11 +0100

    add new FileExistsException for deprecated Illuminate\Contracts\Filesystem\FileExistsException

---
+++ src/Exceptions/FileExistsException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cybex\Lodor\Exceptions;
+
+use Exception;
+
+/**
+ * Class FileExistsException
+ *
+ * Thrown if the filename already exists when trying to merge chunks.
+ *
+ * @package Cybex\Lodor\Exceptions
+ */
+class FileExistsException extends Exception
+{
+    //
+}
